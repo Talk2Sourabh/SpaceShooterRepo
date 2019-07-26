9e4702a19431d468b33661e08d6b13a3d61b8749
Talk2Sourabh <sourabh.singhai1990@gmail.com>
2019-07-26 19:24:39 +0530

    PlayerMovementAndBulltes

---
+++ SpaceShooter/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float _bulletSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * _bulletSpeed); 
+
+        if(transform.position.y >= 7f)
+        {
+           
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerBehavoiur.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavoiur : MonoBehaviour
+{
+    [SerializeField]
+    private float _playerMovementSpeed = 5f;
+
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    [SerializeField]
+    private float _fireRate = 0.1f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        CalclulateMovement();
+        ShootBullet();    
+    }
+
+    void CalclulateMovement()
+    {
+
+        float _horizontal = Input.GetAxis("Horizontal");
+        float _vertical = Input.GetAxis("Vertical");
+        Vector3 _direction = new Vector3(_horizontal, _vertical, 0f);
+        transform.Translate(_direction * Time.deltaTime * _playerMovementSpeed);
+
+        float _xValue = transform.position.x;
+        if (_xValue >= 9)
+        {
+            transform.position = new Vector3(-9f, transform.position.y, 0f);
+        }
+        else if (_xValue <= -9)
+        {
+            transform.position = new Vector3(9f, transform.position.y, 0f);
+        }
+
+        float _yValue = Mathf.Clamp(transform.position.y, -3.8f, 0f);
+        transform.position = new Vector3(transform.position.x, _yValue, 0f);
+    }
+
+    float _timeGapBtwBullets = 0f;
+    void ShootBullet()
+    {
+        _timeGapBtwBullets += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && _timeGapBtwBullets >= _fireRate)
+        {
+            _timeGapBtwBullets = 0f;
+            GameObject _bullet = Instantiate(_bulletPrefab,);
+        }
+    }
+}
+
